ceb3f38c7e044c1525dc741618d8808556f7ee2a
Khoi Duong <khoiduongbusiness@gmail.com>
2025-04-06 22:41:20 -0400

    added same line path cost, it should work

--- Part5.py
+++ Part5.py
@@ -25,9 +25,6 @@ def build_graph(connections_data, stations_data, goal_station):
 
         connections_graph.add_edge(int(row['station1']), int(row['station2']), weight, line)
 
-        
-        calculate_heuristic(connections_graph, goal_station, stations_data)
-
     return connections_graph
 
 
@@ -39,8 +36,9 @@ def build_graph_2(connections_data, stations_data):
         coordinate1 = get_station_coords(stations_data, row['station1'])
         coordinate2 = get_station_coords(stations_data, row['station2'])
         weight = euclidean_dist(coordinate1,coordinate2)
+        line = row['line']
 
-        connections_graph.add_edge(int(row['station1']), int(row['station2']), weight)
+        connections_graph.add_edge(int(row['station1']), int(row['station2']), weight, line)
 
     return connections_graph
 
@@ -91,11 +89,6 @@ london_stations_data = parse_csv("london_stations.csv")
 
 test_graph = build_graph(london_connections_data,london_stations_data, 70)
 
-
-
-test_dict = station_lines(london_connections_data,london_stations_data)
-print(test_dict)
-
 #print(test_graph.graph)
 #print(test_graph.heuristic)
 
@@ -108,6 +101,7 @@ def calculate_heuristic(graph, goal_station, stations_data):
     return total_heuristic
 
 graph1 = build_graph_2(london_connections_data, london_stations_data)
+
 # Generates all pairs
 def all_pairs_a_star(graph):
     all_paths = {}
@@ -122,5 +116,41 @@ def all_pairs_a_star(graph):
 
     return all_paths            
 
-print(all_pairs_a_star(graph1))
+#print(all_pairs_a_star(graph1))
 
+print(graph1.graph)
+
+def same_line(graph, stations_line_data, stations_data):
+    same_line_paths_cost = {}
+
+    for source in graph.graph.keys():
+        for destination in graph.graph.keys():
+            # We check this so that we only count the path once because going from destination to source is also a path the algorithm could take
+            if source < destination:
+                source_lines = stations_line_data.get(str(source))
+                destination_lines = stations_line_data.get(str(destination))
+                # Using basic set intersection method (I did not know this even existed before googling)
+                lines_in_common = source_lines.intersection(destination_lines)
+
+                # Check to see if there are lines in common at all between the pair of stations the loop is currently examining
+                if lines_in_common:
+                    heuristic_value = calculate_heuristic(graph, destination, stations_data)
+                    # Came_from is not used here but it is returned by A* so we just left it there as a dummy variable so that we can grab path
+                    came_from, path = AStar.A_Star(graph,source,destination, heuristic_value)
+
+                    #Check to see if there is even a path returned by A*
+                    if path:
+                        cost = path_cost(path,graph)
+                        same_line_paths_cost[(source,destination)] = {'lines_in_common': lines_in_common, 'path': path, 'total_cost': cost}
+
+    return  same_line_paths_cost
+
+
+# Helper function here to help calculate the cost of a path
+def path_cost(path, graph):
+    cost = 0
+    # Sums up the weight from the source node to the next one in the path and increments for the whole path until the end
+    for i in range (len(path) - 1):
+        cost += graph.weight[(path[i],path[i + 1])]
+    
+    return cost
